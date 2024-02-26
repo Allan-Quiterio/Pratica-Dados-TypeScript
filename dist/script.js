78a938a104602357ae017d5f81a86783532701f5
Allan-Quiterio <allanquiterio100@gmail.com>
2024-02-26 20:33:39 -0300

    feat: create fetchData

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,8 @@
-"use strict";
+import fetchData from "./fetchData.js";
+async function handleData() {
+    const data = await fetchData("https://api.origamid.dev/json/transacoes.json");
+    if (data) {
+    }
+}
+handleData();
 //# sourceMappingURL=script.js.map
