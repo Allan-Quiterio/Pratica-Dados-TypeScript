c9b7e8c142ed3799412b606e54aca59e440610ac
Allan-Quiterio <allanquiterio100@gmail.com>
2024-02-27 19:31:13 -0300

    feat: countBy payments and status

--- src/Statistics.ts
+++ src/Statistics.ts
@@ -1,3 +1,5 @@
+import countBy from "./countBy.js";
+
 type TransacaoValor = ITransacao & {
   valor: number;
 };
@@ -9,14 +11,26 @@ function filtrarValor(transacao: ITransacao): transacao is TransacaoValor {
 export default class Estatisticas {
   private transacoes;
   total;
+  pagamento;
+  status;
   constructor(transacoes: ITransacao[]) {
     this.transacoes = transacoes;
     this.total = this.setTotal();
+    this.pagamento = this.setPagamento();
+    this.status = this.setStatus();
   }
 
   private setTotal() {
     return this.transacoes.filter(filtrarValor).reduce((acc, item) => {
       return acc + item.valor;
     }, 0);
+  }
+
+  private setPagamento() {
+    return countBy(this.transacoes.map(({ pagamento }) => pagamento));
+  }
+
+  private setStatus() {
+    return countBy(this.transacoes.map(({ status }) => status));
   }
 }
